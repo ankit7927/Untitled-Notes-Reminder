681a936efb38924be06f42dea08ef4e0eef6d794
Ankit <code7lancer@gmail.com>
2022-12-27 04:42:29 -0800

    created NotesViewModel

---
+++ app/src/main/java/com/x64tech/notesreminder/database/NotesViewModel.java
@@ -0,0 +1,30 @@
+package com.x64tech.notesreminder.database;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class NotesViewModel extends AndroidViewModel {
+
+    private final NotesRepo notesRepo;
+    private final LiveData<List<NotesModel>> listLiveData;
+
+
+    public NotesViewModel(@NonNull Application application) {
+        super(application);
+        notesRepo = new NotesRepo(application);
+        listLiveData=notesRepo.getAllNotes();
+    }
+
+    public LiveData<List<NotesModel>> getAllNotes(){ return listLiveData; }
+
+    public void insert(NotesModel notesModel) { notesRepo.insert(notesModel); }
+
+    public void update(NotesModel notesModel) { notesRepo.update(notesModel); }
+
+    public void delete(NotesModel notesModel) { notesRepo.delete(notesModel); }
+}
