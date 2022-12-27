2f162b63a232019758bd2a61b3795c99472a7b2e
Ankit <code7lancer@gmail.com>
2022-12-27 04:35:46 -0800

    created NotesDatabase

---
+++ app/src/main/java/com/x64tech/notesreminder/database/NotesDatabase.java
@@ -0,0 +1,33 @@
+package com.x64tech.notesreminder.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = NotesModel.class, version = 1)
+public abstract class NotesDatabase extends RoomDatabase {
+    public abstract NotesDAO notesDAO();
+    private static volatile NotesDatabase Instance;
+    static final ExecutorService databaseWriteExecutor =
+            Executors.newFixedThreadPool(4);
+
+    static NotesDatabase getInstance(final Context context){
+        if (Instance == null){
+            synchronized (NotesDatabase.class){
+                if (Instance == null){
+                    Instance = Room.databaseBuilder(context.getApplicationContext(),
+                            NotesDatabase.class, "Notes_Database")
+                            .allowMainThreadQueries()
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return Instance;
+    }
+}
