1266f4f208410a7794d19909d57f59eb9ca7dea1
Ankit <code7lancer@gmail.com>
2022-12-27 11:58:10 -0800

    created about page

---
+++ app/src/main/java/com/x64tech/notesreminder/pages/AboutUsActivity.java
@@ -0,0 +1,16 @@
+package com.x64tech.notesreminder.pages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.x64tech.notesreminder.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
+}
