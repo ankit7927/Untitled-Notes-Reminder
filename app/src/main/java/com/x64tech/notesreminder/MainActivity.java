0df1138c1849e1948299dd2a891bf42dc1e4ce0b
Ankit <code7lancer@gmail.com>
2022-12-27 06:56:36 -0800

    added click listener to add button

--- app/src/main/java/com/x64tech/notesreminder/MainActivity.java
+++ app/src/main/java/com/x64tech/notesreminder/MainActivity.java
@@ -1,14 +1,37 @@
 package com.x64tech.notesreminder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.x64tech.notesreminder.pages.NoteActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
+    RecyclerView notesRecycleView;
+    FloatingActionButton addNotesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initVars();
+
+        addNotesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(), NoteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initVars() {
+        notesRecycleView = findViewById(R.id.notesRecycleView);
+        addNotesButton = findViewById(R.id.addNotesButton);
     }
 }
