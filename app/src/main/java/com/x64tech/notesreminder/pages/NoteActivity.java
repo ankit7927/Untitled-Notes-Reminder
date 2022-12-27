0b89e757323dba4f95dc5a5709800ae030bf282c
Ankit <code7lancer@gmail.com>
2022-12-27 07:15:17 -0800

    done till saving note

--- app/src/main/java/com/x64tech/notesreminder/pages/NoteActivity.java
+++ app/src/main/java/com/x64tech/notesreminder/pages/NoteActivity.java
@@ -1,16 +1,49 @@
 package com.x64tech.notesreminder.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import com.x64tech.notesreminder.R;
+import com.x64tech.notesreminder.database.NotesModel;
+import com.x64tech.notesreminder.database.NotesViewModel;
 
 public class NoteActivity extends AppCompatActivity {
 
+    private NotesViewModel notesViewModel;
+    private EditText noteTitle, noteBody;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        initVars();
+    }
+
+    private void initVars() {
+        noteTitle = findViewById(R.id.noteTitle);
+        noteBody = findViewById(R.id.noteBody);
+
+        notesViewModel = new ViewModelProvider(this).get(NotesViewModel.class);
+    }
+
+    private void save(){
+        if (!(noteTitle.getText().toString().trim().equals("")
+        && noteBody.getText().toString().trim().equals(""))){
+            NotesModel notesModel = new NotesModel();
+            notesModel.setTitle(noteTitle.getText().toString().trim());
+            notesModel.setBody(noteBody.getText().toString().trim());
+
+            notesViewModel.insert(notesModel);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        save();
     }
 }
